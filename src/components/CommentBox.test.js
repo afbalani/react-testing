d2e47d5a4d6b86cfbeba6b8caf5dddfd87509313
adqt0019 <alesandro.balani@adiq.com.br>
2020-02-06 08:58:28 -0300

    reducers

--- src/components/CommentBox.test.js
+++ src/components/CommentBox.test.js
@@ -17,20 +17,27 @@ it('Shows text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1)
 })
 
-it('Value from text area stored in comment status and shows in text area', () => {
+describe('functionalities ', () => {
   const texto = 'teste texto'
-  wrapped.find('textarea').simulate('change', {target: { value: texto}})
-  wrapped.update()
-  expect(wrapped.find('textarea').prop('value')).toEqual(texto)
-  expect(wrapped.state('comment')).toEqual(texto)
-})
 
-it('OnSubmit text area and comment status is cleaned', () => {
-  const texto = 'teste texto 2'
-  wrapped.setState({ comment: texto })
-  expect(wrapped.find('textarea').prop('value')).toEqual(texto)
-  wrapped.find('form').simulate('submit', { preventDefault:  () => {}})
-  wrapped.update()
-  expect(wrapped.find('textarea').prop('value')).toEqual('')
-  expect(wrapped.state('comment')).toEqual('')
+  beforeEach(() => {
+    //any logic
+  })
+
+  it('Value from text area stored in comment status and shows in text area', () => {
+    wrapped.find('textarea').simulate('change', {target: { value: texto}})
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual(texto)
+    expect(wrapped.state('comment')).toEqual(texto)
+  })
+  
+  it('OnSubmit text area and comment status is cleaned', () => {
+    wrapped.setState({ comment: texto })
+    expect(wrapped.find('textarea').prop('value')).toEqual(texto)
+    wrapped.find('form').simulate('submit', { preventDefault:  () => {}})
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+    expect(wrapped.state('comment')).toEqual('')
+  })
+
 })
