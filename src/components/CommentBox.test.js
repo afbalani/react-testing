df393023ddc95c0687a56e64a3a5e53882a4782d
adqt0019 <alesandro.balani@adiq.com.br>
2020-02-07 20:03:14 -0300

    root

--- src/components/CommentBox.test.js
+++ src/components/CommentBox.test.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { shallow} from 'enzyme'
+import { mount} from 'enzyme'
+import Root from 'Root'
 import CommentBox from 'components/CommentBox'
 
 let wrapped
 
 beforeEach(() => {
-    wrapped = shallow(<CommentBox />)  
+    wrapped = mount(
+      <Root>
+        <CommentBox />
+      </Root>
+    )  
 })
 
 afterEach(() => {
@@ -21,23 +26,19 @@ describe('functionalities ', () => {
   const texto = 'teste texto'
 
   beforeEach(() => {
-    //any logic
+    wrapped.find('textarea').simulate('change', {target: { value: texto}})
+    wrapped.update()
   })
 
   it('Value from text area stored in comment status and shows in text area', () => {
-    wrapped.find('textarea').simulate('change', {target: { value: texto}})
-    wrapped.update()
     expect(wrapped.find('textarea').prop('value')).toEqual(texto)
-    expect(wrapped.state('comment')).toEqual(texto)
   })
   
   it('OnSubmit text area and comment status is cleaned', () => {
-    wrapped.setState({ comment: texto })
     expect(wrapped.find('textarea').prop('value')).toEqual(texto)
     wrapped.find('form').simulate('submit', { preventDefault:  () => {}})
     wrapped.update()
     expect(wrapped.find('textarea').prop('value')).toEqual('')
-    expect(wrapped.state('comment')).toEqual('')
   })
 
 })
